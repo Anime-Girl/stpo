eedc0c54d39589c99f718388d62bb9a6bb97a135
Anime-Girl <yoroshikuqq@mail.ru>
2021-02-28 03:34:37 +0300

    Добавьте файлы проекта.

---
+++ Funcs.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+	public class Funcs
+	{
+		public int countCondNums(int[] nums)
+		{
+			int count = 0;
+			for (int i = 0; i < nums.Length; i++)
+			{
+				if (checkAverageLTSeven(nums[i]) && checkMaxSeven(nums[i])) count++;
+			}
+			return count;
+		}
+
+		public bool checkAverageLTSeven(int num)
+		{
+			if ((num % 10 + (num / 10) % 10 + (num / 100) % 10 + num / 1000) / 4.0 < 7.0) return true;
+			return false;
+		}
+
+		public bool checkMaxSeven(int num)
+		{
+			int max = 0;
+			while (num > 0)
+			{
+				if (num % 10 > max) max = num % 10;
+				num /= 10;
+			}
+			if (max == 7) return true;
+			return false;
+		}
+
+		public int[] getNumsArray(string arrayString)
+		{
+			List<string> splitedArray = arrayString.Split(' ').ToList();
+			splitedArray = splitedArray.Where(str => str != "").ToList();
+
+			if (splitedArray.Count != 10) return null;
+			int[] array = new int[10];
+
+			for (int i = 0; i < array.Length; i++)
+			{
+				array[i] = Int32.Parse(splitedArray[i]);
+				if (array[i] < 1000 || array[i] > 9999) return null;
+			}
+			return array;
+		}
+
+	}
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace WpfApp1
+{
+	public partial class MainWindow : Window
+	{
+		public MainWindow() {
+			InitializeComponent();
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e) {
+			WpfApp1.Funcs f = new Funcs();
+			ResultBox.Content = "Ответ :";
+			string arrayString = EntryBox.Text;
+			int[] nums = f.getNumsArray(arrayString);
+			if (nums == null) {
+				ResultBox.Content = ResultBox.Content + " Числа введены неверно!";
+			} else {
+				EntryBox.Text = string.Join(" ", nums);
+				int count = f.countCondNums(nums);
+				if (count != 0) ResultBox.Content = ResultBox.Content + " " + count.ToString();
+				else ResultBox.Content = ResultBox.Content + " Нет чисел, удовлетворяющих заданию";
+			}
+		}
+
+		private void EntryBox_GotFocus(object sender, RoutedEventArgs e) {
+			if (EntryBox.Text == "Поле ввода") {
+				EntryBox.Text = "";
+				EntryBox.Foreground = new SolidColorBrush(Color.FromRgb(0x0, 0x0, 0x0));
+			}
+		}
+
+		private void EntryBox_LostFocus(object sender, RoutedEventArgs e) {
+			if (EntryBox.Text == "") { 
+				EntryBox.Text = "Поле ввода";
+				EntryBox.Foreground = new SolidColorBrush(Color.FromRgb(0x97, 0x97, 0x87));
+			}
+		}
+	}
+}
